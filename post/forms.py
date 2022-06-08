918a213414bb377f0a363a6759118c01b70af0f6
brianmwevi <mwevibrian@gmail.com>
2022-06-08 12:00:30 +0300

    ft:forms -- add comment form

--- post/forms.py
+++ post/forms.py
@@ -6,3 +6,9 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('image', 'captions')
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
